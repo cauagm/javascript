833c1d2c9c839fb97df04315fa82415e2e1cc917
Cauã Granzotti <cauaznmarques10@gmail.com>
2026-03-12 18:34:18 -0300

    estrutura de repeticao

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+//do while
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c <=6)
+
+
+
+
+/*
+while
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
